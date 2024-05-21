9e85b61ccbfdaaa2e0a4df68b354610ea8698c97
nilzkee <168503667+nilzkee@users.noreply.github.com>
2024-05-21 14:11:32 +0700

    Initial Commit

---
+++ ThinkMealApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThinkMealApp Watch App
+//
+//  Created by Nila Eleora Putri Sianturi on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThinkMealApp Watch App/ThinkMealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkMealAppApp.swift
+//  ThinkMealApp Watch App
+//
+//  Created by Nila Eleora Putri Sianturi on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ThinkMealApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
